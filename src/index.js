3fea2e16e2983c68452d4ff2603fe6f8fe6a1fd7
Dzima <d.propolsky@gmail.com>
2020-10-20 16:21:26 +0300

    fix: used babel for morse-decoder function

--- src/index.js
+++ src/index.js
@@ -40,33 +40,30 @@
     function decode(expr) {
         let initialString = expr;
         let reg = /([0-1\*]{10})/;
-        const dotsDashCoder = (elem) => {
-            let dotsDashResult = '';             
-            // если в строке нету *, т.е. потенциального пробела, то проверяем
+        const dotsDashCoder = elem => {
+            let dotsDashResult = '';
             if (elem.indexOf('*') === -1 && elem.indexOf('1') !== -1) {
                 let fromThisPosition = 0;
                 while (fromThisPosition <= elem.length - 1) { 
-                    let oneSearcher = elem.indexOf(1, fromThisPosition); // ищу 1
-                    if (elem[oneSearcher + 1] === '0') { //если следующий за ним элемент 0, то 10
-                        dotsDashResult += '.'; // 10 это точка
+                    let oneSearcher = elem.indexOf(1, fromThisPosition);
+                    if (elem[oneSearcher + 1] === '0') { 
+                        dotsDashResult += '.'; 
                         fromThisPosition = oneSearcher + 2;
-                    } else if (elem[oneSearcher + 1] === '1'){ //если следующий за ним элемент 1, то 11
-                        dotsDashResult += '-'; // 11 это тире
+                    } else if (elem[oneSearcher + 1] === '1'){
+                        dotsDashResult += '-'; 
                         fromThisPosition = oneSearcher + 2;
                     }; 
                 };
             };
             return dotsDashResult;
-        };
-        // where search   [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", "-----"]
-        const isMorseCode = (element) => {
+        };        
+        const isMorseCode = element => {
             if (MORSE_TABLE.hasOwnProperty(element) === true) {
                 return MORSE_TABLE[element];
             } else {
                 return ' ';
             };    
-        };
-        // what search   ["....", ".", ".-..", ".-..", "---", "", ".--", "---", ".-.", ".-..", "-.."]
+        };       
         let arrOfEveryTenSymbols = initialString.split(reg).filter(element => element !== '').map(dotsDashCoder);
         let translateText = arrOfEveryTenSymbols.map(isMorseCode).join('');
         return translateText;
